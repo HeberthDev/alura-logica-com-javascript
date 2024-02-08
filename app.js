a1c57c2180150e1aca64f2f379a4343b4661f8b1
HeberthDev <heberthdeveloper@gmail.com>
2024-02-08 08:48:39 -0300

    Número aleatório

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 alert('Boas vindas ao jogo do número secreto.');
 
-let numeroSecreto = parseInt(Math.random() * 10);
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
