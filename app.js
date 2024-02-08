559f9d458946353c4b608fc8eb5a6f90314516d6
HeberthDev <heberthdeveloper@gmail.com>
2024-02-08 08:01:06 -0300

    Tentativas

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ alert('Boas vindas ao jogo do número secreto.');
 let numeroSecreto = 9;
 console.log(numeroSecreto);
 let chute;
+let tentativas = 1;
 
 //enquanto 
 while (chute != numeroSecreto) {
@@ -10,12 +11,14 @@ while (chute != numeroSecreto) {
 
     //se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert (`Isso aí! Você descobriu o número secreto (${numeroSecreto})`);
+        alert (`Isso aí! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} tentativas.`);
     } else {
         if (chute < numeroSecreto) {
             alert(`O número secreto é maior que ${chute}`);
         } else {
             alert(`O número secreto é menor que ${chute}`);
         }
+
+        tentativas++;
     }
 }
